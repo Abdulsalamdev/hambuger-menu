b5598e3bc21cc57e40bb7b9abfabdcb529398a59
Abdulsalamdev <abdulsalamakinyoola@gmail.com>
2023-08-16 17:54:49 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const image = document.querySelector('.image');
+const absolute = document.querySelector('.none');
+
+image.addEventListener('click', function () {
+   absolute.classList.toggle('show')
+   
+})
